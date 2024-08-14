692b0b1972439c81422b15903c920296d531148d
soyu <satori562003@gmail.com>
2024-08-14 15:11:27 +0700

    feat: add jwt and authorize, add keys

---
+++ Business Logic Layer/Models/JwtSettingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic_Layer.Models
+{
+	public class JwtSettingModel
+	{
+		public string SecretKey { get; set; }
+	}
+}
--- SpotifyPool/Program.cs
+++ SpotifyPool/Program.cs
@@ -8,6 +8,11 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using SpotifyPool.JIRA_REST_API.Issues;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.Google;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +21,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Spotify Pool", Version = "v1" });
+
+	// Add JWT Authentication
+	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Name = "Authorization",
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer",
+		BearerFormat = "JWT",
+		In = ParameterLocation.Header,
+		Description = "Enter 'Bearer' [space] and then your token",
+	});
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Bearer"
+							}
+						},
+						new string[] {}
+					}
+				});
+});
 
 //Config the database
 builder.Services.Configure<MongoDBSetting>(builder.Configuration.GetSection("MongoDBSettings"));
@@ -30,20 +63,47 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Config the Google Identity
 builder.Services.AddAuthentication(options =>
 {
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    //options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	//options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	////options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+	//options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddCookie().AddGoogle(options =>
 {
-    options.ClientId = builder.Configuration.GetSection("Authentication:Google:ClientId").Value;
-    options.ClientSecret = builder.Configuration.GetSection("Authentication:Google:ClientSecret").Value;
-    options.CallbackPath = "/api/authentication/signin-google"; // Đường dẫn Google sẽ chuyển hướng sau khi xác thực
+	options.ClientId = builder.Configuration.GetSection("Authentication:Google:ClientId").Value;
+	options.ClientSecret = builder.Configuration.GetSection("Authentication:Google:ClientSecret").Value;
+	options.CallbackPath = "/api/authentication/signin-google"; // Đường dẫn Google sẽ chuyển hướng sau khi xác thực
 
-    options.Scope.Add("profile");
-    options.Scope.Add("email");
-    options.Scope.Add("openid");
+	options.Scope.Add("profile");
+	options.Scope.Add("email");
+	options.Scope.Add("openid");
 
-    options.ClaimActions.MapJsonKey("urn:google:picture", "picture", "url");
+	options.ClaimActions.MapJsonKey("urn:google:picture", "picture", "url");
+}).AddJwtBearer(opt =>
+{
+	opt.TokenValidationParameters = new TokenValidationParameters
+	{
+		//tự cấp token
+		ValidateIssuer = false,
+		ValidateAudience = false,
+		ValidateLifetime = true,
+
+		//ký vào token
+		ValidateIssuerSigningKey = true,
+		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:SecretKey"])),
+
+		ClockSkew = TimeSpan.Zero
+	};
+});
+
+builder.Services.AddAuthorization(options =>
+{
+	options.AddPolicy("GoogleOrJwt", policy =>
+	{
+		policy.AddAuthenticationSchemes(GoogleDefaults.AuthenticationScheme, JwtBearerDefaults.AuthenticationScheme);
+		policy.RequireAuthenticatedUser();
+	});
 });
 
 // Config the email sender (SMTP)
@@ -75,6 +135,8 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
