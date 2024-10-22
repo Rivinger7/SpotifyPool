da12e6c9ccebe6b0737d1a3199bf98f03c4e2753
Phạm Duy Hoàng <hoangpdse172046@fpt.edu.vn>
2024-10-22 18:18:00 +0700

    fix: delete redundant file

--- Back-end/BusinessLogicLayer.ModelView/Service Model Views/Images/Request/AvatarUserRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BusinessLogicLayer.ModelView.Service_Model_Views.Images.Request
-{
-	public class AvatarUserRequest
-	{
-		public string? URL { get; set; }
-		public int Height { get; set; } = 500;
-		public int Width { get; set; } = 313;
-	}
-}
