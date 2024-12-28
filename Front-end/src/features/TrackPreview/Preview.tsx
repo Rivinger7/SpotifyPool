1d9f934deae33066027e6b901c3790a39eea39a6
anphuchoa291204 <142864120+hoapooh@users.noreply.github.com>
2024-12-28 17:56:58 +0700

    Features disabled for unauthenticated users
    - Disable some feature for users who are not logged in to the web.
       + TracksComponent
       + Create Playlist
    - Show message when user click on prohibited components.

--- Front-end/src/features/TrackPreview/Preview.tsx
+++ Front-end/src/features/TrackPreview/Preview.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const Preview = () => {
 	return (
 		<div className={"now-playing-bar-container h-[68px]"}>
@@ -10,11 +12,14 @@ const Preview = () => {
 							needed.
 						</p>
 					</div>
-					<button className="bg-transparent border-0 cursor-pointer text-center touch-manipulation transition-all align-middle rounded-full group">
-						<span className="bg-white text-black flex items-center justify-center rounded-full font-bold p-2 pl-8 pr-8 min-h-12 group-hover:scale-105 group-hover:bg-[#f0f0f0]">
-							Sign up free
-						</span>
-					</button>
+
+					<Link to={"/signup"}>
+						<button className="bg-transparent border-0 cursor-pointer text-center touch-manipulation transition-all align-middle rounded-full group">
+							<span className="bg-white text-black flex items-center justify-center rounded-full font-bold p-2 pl-8 pr-8 min-h-12 group-hover:scale-105 group-hover:bg-[#f0f0f0]">
+								Sign up for free
+							</span>
+						</button>
+					</Link>
 				</div>
 			</footer>
 		</div>
