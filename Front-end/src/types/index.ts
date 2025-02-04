f2733561cfc6b95136282622dd9b808a00489faf
hoapooh <142864120+hoapooh@users.noreply.github.com>
2025-02-04 22:16:36 +0700

    Revert "SP-53 Track details UI (right sidebar)"

--- Front-end/src/types/index.ts
+++ Front-end/src/types/index.ts
@@ -19,7 +19,7 @@ export interface Track {
 	duration: number
 	durationFormated: string
 	images: Images[]
-	artists?: Artists[] | []
+	artists?: Artists[] | undefined
 }
 
 export interface Playlist {
