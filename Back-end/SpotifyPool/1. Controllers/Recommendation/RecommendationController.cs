36524da6d88fd544ebb6d888ca6752959ba73024
Okasu007 <Rivinger7@gmail.com>
2025-02-21 18:26:21 +0700

    fix naming conversion reccomendation api

--- Back-end/SpotifyPool/1. Controllers/Recommendation/RecommendationController.cs
+++ Back-end/SpotifyPool/1. Controllers/Recommendation/RecommendationController.cs
@@ -4,6 +4,7 @@ using BusinessLogicLayer.ModelView.Service_Model_Views.AudioFeatures.Request;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SetupLayer.Enum.Services.Reccomendation;
 using SetupLayer.Enum.Services.User;
 
 namespace SpotifyPool._1._Controllers.Recommendation
@@ -20,7 +21,7 @@ namespace SpotifyPool._1._Controllers.Recommendation
         /// </summary>
         /// <param name="audioFeaturesRequest"></param>
         /// <returns></returns>
-        [AllowAnonymous, HttpPost("botpress/searching")]
+        [AllowAnonymous, HttpPost("tracks")]
         public async Task<IActionResult> GetRecommendedSongByBotpressSearching([FromBody] AudioFeaturesRequest audioFeaturesRequest)
         {
             var result = await _recommendationService.GetRecommendations(audioFeaturesRequest, RecommendationBLL.CalculateWeightedEulideanDisctance);
@@ -28,39 +29,37 @@ namespace SpotifyPool._1._Controllers.Recommendation
         }
 
         /// <summary>
-        /// Sử dụng thuật toán Weighted Euclidean Distance
+        /// Đề xuất bài hát dựa trên trackId
         /// </summary>
         /// <param name="trackId"></param>
+        /// <param name="algorithm"></param>
         /// <returns></returns>
-        [Authorize(Roles = nameof(UserRole.Customer)), HttpGet("{trackId}/Weighted-Euclidean-Distance")]
-        public async Task<IActionResult> GetRecommendedSongByWeightedEulideanDisctance(string trackId)
+        [Authorize(Roles = nameof(UserRole.Customer)), HttpGet("tracks/{trackId}")]
+        public async Task<IActionResult> GetRecommendedSongByTrackId([FromRoute] string trackId, [FromQuery] Algorithm algorithm = Algorithm.CosineSimilarity)
         {
-            var result = await _recommendationService.GetRecommendations(trackId, RecommendationBLL.CalculateWeightedEulideanDisctance);
-            return Ok(result);
+            return algorithm switch
+            {
+                Algorithm.CosineSimilarity => Ok(await _recommendationService.GetRecommendations(trackId, RecommendationBLL.CalculateCosineSimilarity)),
+                Algorithm.WeightedEuclideanDistance => Ok(await _recommendationService.GetRecommendations(trackId, RecommendationBLL.CalculateWeightedEulideanDisctance)),
+                _ => BadRequest("Invalid algorithm"),
+            };
         }
 
         /// <summary>
-        /// Sử dụng thuật toán Cosine Similarity
+        /// Đề xuất bài hát dựa trên danh sách trackIds
         /// </summary>
-        /// <param name="trackId"></param>
+        /// <param name="trackIds"></param>
+        /// <param name="algorithm"></param>
         /// <returns></returns>
-        [Authorize(Roles = nameof(UserRole.Customer)), HttpGet("{trackId}/Cosine-Similarity")]
-        public async Task<IActionResult> GetRecommendedSongByCosineSimilarity(string trackId)
+        [Authorize(Roles = nameof(UserRole.Customer)), HttpGet("tracks")]
+        public async Task<IActionResult> GetRecommendedSongsByCosineSimilarity([FromQuery] IEnumerable<string> trackIds, [FromQuery] Algorithm algorithm = Algorithm.CosineSimilarity)
         {
-            var result = await _recommendationService.GetRecommendations(trackId, RecommendationBLL.CalculateCosineSimilarity);
-            return Ok(result);
-        }
-
-        /// <summary>
-        /// Sử dụng thuật toán Cosine Similarity
-        /// </summary>
-        /// <param name="trackId"></param>
-        /// <returns></returns>
-        [Authorize(Roles = nameof(UserRole.Customer)), HttpGet("tracks/Cosine-Similarity")]
-        public async Task<IActionResult> GetRecommendedSongsByCosineSimilarity([FromQuery] IEnumerable<string> trackId)
-        {
-            var result = await _recommendationService.GetManyRecommendations(trackId, RecommendationBLL.CalculateCosineSimilarity);
-            return Ok(result);
+            return algorithm switch
+            {
+                Algorithm.CosineSimilarity => Ok(await _recommendationService.GetManyRecommendations(trackIds, RecommendationBLL.CalculateCosineSimilarity)),
+                Algorithm.WeightedEuclideanDistance => Ok(await _recommendationService.GetManyRecommendations(trackIds, RecommendationBLL.CalculateWeightedEulideanDisctance)),
+                _ => BadRequest("Invalid algorithm"),
+            };
         }
     }
 }
