f073714032fa7d603ba5694b968d5abaadeaba4d
Okasu007 <Rivinger7@gmail.com>
2025-03-11 10:39:51 +0700

    remove swagger from production environment

--- Back-end/BusinessLogicLayer.Interface/Services Interface/JWTs/IJwtBLL.cs
+++ Back-end/BusinessLogicLayer.Interface/Services Interface/JWTs/IJwtBLL.cs
@@ -1,17 +1,16 @@
-﻿using BusinessLogicLayer.ModelView.Service_Model_Views.JWTs.Request;
-using DataAccessLayer.Repository.Entities;
-using System.IdentityModel.Tokens.Jwt;
+﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 namespace BusinessLogicLayer.Interface.Services_Interface.JWTs
 {
     public interface IJwtBLL
     {
-        void RevokeToken(string Id);
+        Task RevokeToken(string Id);
         void RefreshAccessToken(out string newAccessToken, out string newRefreshToken, out ClaimsPrincipal principal, string oldRefreshToken);
         string GenerateJWTTokenForConfirmEmail(string email, string encrpytedToken);
         JwtSecurityToken DecodeToken(string token);
         void GenerateAccessToken(IEnumerable<Claim> claims, string userId, out string accessToken, out string refreshToken);
         ClaimsPrincipal ValidateToken(string token);
+        void Dispose();
     }
 }
