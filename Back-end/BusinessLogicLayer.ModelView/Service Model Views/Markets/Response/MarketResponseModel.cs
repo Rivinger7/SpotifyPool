f6937923fc860883d519f851fb2cbedbda1eba0a
TDHanie <luongngocthuyduongvt@gmail.com>
2025-03-15 15:32:31 +0700

    Admin fix phần update: không dùng Mapper nữa, chuyển sang dùng Builders<User> để tối ưu vấn đề cập nhật các fields cần thiết
    - Xóa thư viện không dùng tới

--- Back-end/BusinessLogicLayer.ModelView/Service Model Views/Markets/Response/MarketResponseModel.cs
+++ Back-end/BusinessLogicLayer.ModelView/Service Model Views/Markets/Response/MarketResponseModel.cs
@@ -1,13 +1,8 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BusinessLogicLayer.ModelView.Service_Model_Views.Markets.Response
 {
-    public class MarketResponseModel
+	public class MarketResponseModel
     {
         [JsonProperty("markets")]
         public List<string> MarketCode { get; set; } = null!;
