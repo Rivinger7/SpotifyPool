cc5d88cf95d5998eeafaf728493321f76bfdb493
Okasu007 <Rivinger7@gmail.com>
2025-03-25 15:06:23 +0700

    adding artist's profile

--- Back-end/BusinessLogicLayer.Interface/Services Interface/Artists/IArtist.cs
+++ Back-end/BusinessLogicLayer.Interface/Services Interface/Artists/IArtist.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLogicLayer.ModelView.Service_Model_Views.Artists.Request;
+using BusinessLogicLayer.ModelView.Service_Model_Views.Artists.Response;
 using BusinessLogicLayer.ModelView.Service_Model_Views.Authentication.Response;
 using BusinessLogicLayer.ModelView.Service_Model_Views.Tracks.Response;
 
@@ -7,7 +8,9 @@ namespace BusinessLogicLayer.Interface.Services_Interface.Artists
     public interface IArtist
     {
         Task CreateArtist(ArtistRequest artistRequest);
+        Task<ArtistResponseModel> GetArtistByIdAsync(string artistId);
         Task<IEnumerable<TrackResponseModel>> GetOwnTracks(int offset, int limit);
+        Task<IEnumerable<TrackResponseModel>> GetTracksByArtistId(string artistId, int offset, int limit);
         Task<AuthenticatedUserInfoResponseModel> SwitchToUserProfile();
     }
 }
